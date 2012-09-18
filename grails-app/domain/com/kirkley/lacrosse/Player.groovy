4ed6e4367709878db63fa32d099b63902c7997ee
Matthew Kirkley <matt.kirkley@gmail.com>
2012-09-17 21:44:35 -0400

    domain model for Player

---
+++ grails-app/domain/com/kirkley/lacrosse/Player.groovy
@@ -0,0 +1,26 @@
+package com.kirkley.lacrosse
+
+import com.kirkley.lacrosse.Contact
+import com.kirkley.lacrosse.Team
+
+class Player {
+
+    Integer id
+    
+    Contact contact
+    
+    Team team
+
+    Integer age
+
+    //TODO Is this the correct datatype?
+    String position
+
+    static constraints = {
+    }
+
+    static mapping = {
+        id column:'Player_ID'
+        version false
+    }
+}
