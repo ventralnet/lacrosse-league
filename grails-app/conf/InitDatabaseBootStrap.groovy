24ed67e46ba5698438cc11a979a732317bc3f836
Matthew Kirkley <matt.kirkley@gmail.com>
2012-10-09 21:38:23 -0400

    refactoring database populate script

--- grails-app/conf/InitDatabaseBootStrap.groovy
+++ grails-app/conf/InitDatabaseBootStrap.groovy
@@ -6,55 +6,116 @@ import com.kirkley.lacrosse.*
 
 class InitDatabaseBootStrap {
 
+    static {
+        ArrayList.metaClass.random = { 
+            return delegate[0]
+        }
+    }
+
+    def random = new Random()
+
     static final def COACH = Role.COACH
     static final def PLAYER = Role.PLAYER
     static final def ADMIN = Role.ADMINISTRATOR
 
+    static final def TEAM_NAMES = ['Kirks','Tigers','Jags','Lions']
+
+    static final def LAST_NAMES = ['Smith','Jones','Kirkley','Henry','McCloud','Myers','McKnight','Lewis','Black','Matthews','Mu','Smyth']
+    static final def FIRST_NAMES = ['Matt','Jim','Michael','Justin','Valerie','Jacob','David','Chris','Richard']
+
+    def coachRole
+    def parentPlayerRole
+    def administratorRole
+
+    private def randomPhoneNumber() {
+        def n = { numDigits ->
+            random.nextInt(numDigits) 
+        }
+        "(${n(3)})-${n(3)}-${n(4)}"
+    }
+
+    private def randomEmail(firstname,lastname) {
+        "${firstname.toLowerCase()}.${lastname.toLowerCase()}@gmail.com"
+    }
+
+    private def createRoles() {
+        coachRole = new Role(type:COACH)
+        parentPlayerRole = new Role(type:PLAYER)
+        administratorRole = new Role(type:ADMIN)
+        
+        coachRole.save()
+        parentPlayerRole.save()
+        administratorRole.save()
+        return [coachRole,parentPlayerRole,administratorRole]
+    }
+
+    private def createContact(role) {
+        def lastname = LAST_NAMES.random()
+        def firstname = FIRST_NAMES.random()
+        def contactAdmin = new Contact(lastName:lastname,firstName:firstname, 
+                              emailAddress:randomEmail(firstname,lastname), phoneNumber:randomPhoneNumber(), password:"secret",role:administratorRole) 
+        contactAdmin.save()
+        contactAdmin
+    }
+
+    private def createLeagues(contact) {
+        def league = new League(adminContact:contact,name:"Summer League",year:2012,season:"SUMMER")
+        def league2 = new League(adminContact:contact,name:"Winter League",year:2012,season:"WINTER")
+        def league3 = new League(adminContact:contact,name:"Fall League",year:2012,season:"FALL")
+        def league4 = new League(adminContact:contact,name:"Spring League",year:2012,season:"SPRING")
+
+        league.save()
+        league2.save()
+        league3.save()
+        league4.save()
+    }
+
     def init = { servletContext -> 
         switch(GrailsUtil.environment) {
             case "development":
-                def coachRole = new Role(type:COACH)
-                def parentPlayerRole = new Role(type:PLAYER)
-                def administratorRole = new Role(type:ADMIN)
+                def (coachRole,parentPlayerRole,administratorRole) = createRoles()
 
-                coachRole.save()
-                parentPlayerRole.save()
-                administratorRole.save()
+                def contactAdmin = createContact(administratorRole)
+                def contactCoach = createContact(coachRole)
+                def contactCoach2 = createContact(coachRole)
+                def contactPlayer = createContact(parentPlayerRole)
+                def contactPlayer2 = createContact(parentPlayerRole)
+                def contactPlayer3 = createContact(parentPlayerRole)
 
-                def contactAdmin = new Contact(lastName:"Adminie",firstName:"Joe", emailAddress:"joe_admin@gmail.com", phoneNumber:"410-123-4567", password:"secret",role:administratorRole) 
-                def contactCoach = new Contact(lastName:"Coachie",firstName:"Jon", emailAddress:"jon_coach@gmail.com", phoneNumber:"410-222-4567", password:"secret",role:coachRole) 
-                def contactCoach2 = new Contact(lastName:"Coachie2",firstName:"Jont", emailAddress:"jont_coach@gmail.com", phoneNumber:"410-223-4567", password:"secret",role:coachRole) 
-                def contactPlayer = new Contact(lastName:"Kirkley",firstName:"Matt", emailAddress:"m.k@gmail.com", phoneNumber:"410-258-4567", password:"secret",role:parentPlayerRole) 
-                def contactPlayer2 = new Contact(lastName:"Kirk",firstName:"Jim", emailAddress:"m.kkk@gmail.com", phoneNumber:"410-258-4557", password:"secret",role:parentPlayerRole) 
 
-                contactAdmin.save()
-                contactCoach.save()
-                contactCoach2.save()
-                contactPlayer.save()
-                contactPlayer2.save()
 
-                def league = new League(adminContact:contactAdmin,name:"LeagueA",year:2012,season:"FALL")
-                
-                league.save()
-                def team = new Team(name:'Kirks',league:league,coach:contactCoach)
-                def team2 = new Team(name:'TEAM-1',league:league,coach:contactCoach2)
-
-                team.save()
-                team2.save()
-
-                def player1 = new Player(contact:contactPlayer,team:team,age:18,position:"Attack")
-                def player2 = new Player(contact:contactPlayer2,team:team,age:18,position:"Midfield")
-
-                player1.save()
-                player2.save()
-
-                def location = new Location(contact:contactCoach,name:"loc_name",street:"cleghorn",city:"huntvalley",zipCode:"21030")
-
-                location.save()
-
-                def game = new Game(homeTeam:team,awayTeam:team2,location:location,date:2012,time:new Date())
-
-                game.save()
+//                def league = new League(adminContact:contactAdmin,name:"LeagueA",year:2012,season:"FALL")
+//                def league2 = new League(adminContact:contactAdmin,name:"LeagueB",year:2012,season:"SUMMER")
+//                
+//                league.save()
+//                league2.save()
+//
+//                def team = new Team(name:'Kirks',league:league,coach:contactCoach)
+//                def team2 = new Team(name:'TEAM-1',league:league,coach:contactCoach2)
+//                def team3 = new Team(name:'TEAM-2',league:league,coach:contactCoach2)
+//                def team4 = new Team(name:'TEAM-3',league:league,coach:contactCoach2)
+//                def team5 = new Team(name:'TEAM-4',league:league2,coach:contactCoach) 
+//                
+//                team.save()
+//                team2.save()
+//                team3.save()
+//                team4.save()
+//                team5.save()
+//
+//                def player1 = new Player(contact:contactPlayer,team:team,age:21,position:"Attack")
+//                def player2 = new Player(contact:contactPlayer2,team:team,age:18,position:"Midfield")
+//                def player3 = new Player(contact:contactPlayer3,team:team,age:18,position:"Defense")
+//
+//                player1.save()
+//                player2.save()
+//
+//                def location = new Location(contact:contactCoach,name:"loc_name",street:"cleghorn",city:"huntvalley",zipCode:"21030")
+//
+//                location.save()
+//
+//                def game = new Game(homeTeam:team,awayTeam:team2,location:location,date:2012,time:new Date())
+//
+//                game.save()
                     
             break
         } 
