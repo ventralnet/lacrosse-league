02f07c9e9e6e73a41aa2ec05e24e08a32bdac232
Matthew Kirkley <matt.kirkley@gmail.com>
2012-12-05 16:33:13 -0500

    mysql connection pool test query and eviction policy

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,14 @@
 // Place your Spring DSL code here
 beans = {
-}
+  dataSource(BasicDataSource) {
+    //run the evictor every 30 minutes and evict any connections older than 30 minutes.
+    minEvictableIdleTimeMillis=1800000
+    timeBetweenEvictionRunsMillis=1800000
+    numTestsPerEvictionRun=3
+    //test the connection while its idle, before borrow and return it
+    testOnBorrow=true
+    testWhileIdle=true
+    testOnReturn=true
+    validationQuery="SELECT 1"
+  }
+} 
